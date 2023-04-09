118305828946b3ede780a3792639162480912adf
steven ennis <steven.otieno1@gmail.com>
2023-04-09 16:21:56 +0300

    ft(mortgage cala): and flow conditionals

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,39 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal;
+        float monthlyInterest;
+        int numberOfPayments;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true){
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal <= 1_000_000)
+            break;
+            System.out.println("Enter a value between 1000 and or equal 1000000");
+        }
 
-        System.out.print("Annual interest rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT/MONTHS_IN_YEAR;
+        while(true){
+            System.out.print("Annual interest rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >=1 && annualInterest <=30){
+                monthlyInterest = annualInterest / PERCENT/MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        while(true){
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if(years >= 1 && years <= 30){
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))/(Math.pow(1 + monthlyInterest, numberOfPayments) -1);
 
